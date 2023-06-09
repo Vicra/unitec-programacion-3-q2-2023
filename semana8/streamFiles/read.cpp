21f6c1db9997b083b28c715ec1ebe04f26f0f9bd
Vicra <honvramirez@gmail.com>
2023-06-08 19:45:12 -0600

    read relative path

--- semana8/streamFiles/read.cpp
+++ semana8/streamFiles/read.cpp
@@ -9,20 +9,37 @@ using std::ifstream;
 
 int main(int argc, char const *argv[])
 {
-  ifstream i;
-  // paso 1: abrir/crear archivo a+
-  i.open("data.txt");
+  try {
+    ifstream i;
+    // paso 1: abrir/crear archivo a+
+    i.open("../files/content.txt");
 
-  // paso 2 : leer una linea
-  string a;
-  i >> a;
+    // paso 2 : leer una linea
+    // for( string line; getline( i, line, ',' ); )
+    // {
+    //   cout << line << endl;
+    // }
 
-  for( string line; getline( i, line ); )
-  {
-    cout << line << endl;
-  }
+    // for( string line; getline( i, line, char(10)); )
+    // {
+    //   cout << line << endl;
+    // }
 
-  // paso 3: cerrar el archivo
-  i.close();
+    for( string line; getline( i, line, char(32)); )
+    {
+      cout << line << endl;
+    }
+
+    
+
+    // paso 3: cerrar el archivo
+    i.close();
+  } catch(string e) {
+    cout << e;
+  }catch (const std::exception& ex) {
+    // cout << (string)ex;
+  } catch (const std::string& ex) {
+    cout << ex;
+  } 
   return 0;
 }
