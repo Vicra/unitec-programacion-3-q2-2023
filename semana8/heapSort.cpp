6b28df947fbb406e14929e809e083c80322c1ce7
Vicra <honvramirez@gmail.com>
2023-06-09 19:46:44 -0600

    insert sort

--- semana8/heapSort.cpp
+++ semana8/heapSort.cpp
@@ -8,10 +8,9 @@ void heapify(int arr[], int n, int i) {
   int left = 2 * i + 1;
   int right = 2 * i + 2;
 
-  if (left < n && arr[left] > arr[largest])
-    largest = left;
-
-  if (right < n && arr[right] > arr[largest])
+  if (left < n && arr[left] > arr[largest]) 
+    largest = left; 
+  if (right < n && arr[right] > arr[largest]) 
     largest = right;
 
   // Swap and continue heapifying if root is not largest
